8a59d0c816446cc3b60336ca8a2d3a99650fdd50
onur <ionuraygan@gmail.com>
2022-11-09 23:47:20 +0300

    update

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 import { processRequest } from "zod-express-middleware";
 import logger from './logger';
 
-const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017//youtube-clone"
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/youtube-clone"
 
 export async function connectDatabase() {
     try{
