6f74973219668ae1fcf2a644b5edfe34e0f27c07
onur <ionuraygan@gmail.com>
2022-11-17 22:14:09 +0300

    current user context

--- client/api/index.tsx
+++ client/api/index.tsx
@@ -17,4 +17,13 @@ export function loginUser(payload: { email: string; password: string }) {
   return axios
     .post(authBase, payload, { withCredentials: true })
     .then((res) => res.data);
-} 
+}
+
+export function getMe() {
+  return axios
+    .get(userBase, { withCredentials: true })
+    .then((res) => res.data)
+    .catch(() => {
+      return null;
+    });
+}
--- client/layouts/Home.tsx
+++ client/layouts/Home.tsx
@@ -1,26 +1,46 @@
-import { AppShell, Header, Navbar, Box } from "@mantine/core";
+import { AppShell, Header, Navbar, Box, Anchor } from "@mantine/core";
 import Image from "next/image";
 import React from "react";
-
+import Link from "next/link";
+import { useMe } from "../context/me";
 
 function HomePageLayout({ children }: { children: React.ReactNode }) {
+  const { user, refetch } = useMe();
+  console.log(user);
   return (
-    <AppShell padding="md" 
-        navbar={
-            <Navbar width={{base: 300}} height={500} p="xs">
-                Side items
-            </Navbar>}
-        header={
-            <Header height={60} p="xs">
-                <Box>
-                    <Box>
-                       <Image src="/logo.png" alt="logo" width={100} height={40} />
-                    </Box>
-                </Box> 
-            </Header>
-        }    
-        >
-        {children}
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} height={500} p="xs">
+          Side items
+        </Navbar>
+      }
+      header={
+        <Header height={60} p="xs">
+          <Box sx={() => ({ display: "flex" })}>
+            <Box sx={() => ({ flex: "1" })}> 
+              <Image src="/logo.png" alt="logo" width={100} height={40} />
+            </Box>
+            {!user && (
+              <>
+                <Link href="/auth/login" passHref>
+                  <Anchor ml="lg" mr="lr">
+                    Login
+                  </Anchor>
+                </Link>
+                <Link href="/auth/register" passHref>
+                  <Anchor ml="lg" mr="lr">
+                    Register
+                  </Anchor>
+                </Link>
+              </>
+            )}
+            {user && <p>Upload Video</p>}
+          </Box>
+        </Header>
+      }
+    >
+      {children}
     </AppShell>
   );
 }
