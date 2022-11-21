47e45fecb935ae21bb6deb607c6752e91809c872
onur <ionuraygan@gmail.com>
2022-11-21 17:55:27 +0300

    video context

--- client/layouts/Home.tsx
+++ client/layouts/Home.tsx
@@ -3,46 +3,49 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 import { useMe } from "../context/me";
-import UploadVideo from '../components/UploadVideo'
+import UploadVideo from "../components/UploadVideo";
+import { VideoContextProvider } from "../context/videos";
 
 function HomePageLayout({ children }: { children: React.ReactNode }) {
   const { user, refetch } = useMe();
   console.log(user);
   return (
-    <AppShell
-      padding="md"
-      navbar={
-        <Navbar width={{ base: 300 }} height={500} p="xs">
-          Side items
-        </Navbar>
-      }
-      header={
-        <Header height={60} p="xs">
-          <Box sx={() => ({ display: "flex" })}>
-            <Box sx={() => ({ flex: "1" })}> 
-              <Image src="/logo.png" alt="logo" width={100} height={40} />
+    <VideoContextProvider>
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar width={{ base: 300 }} height={500} p="xs">
+            Side items
+          </Navbar>
+        }
+        header={
+          <Header height={60} p="xs">
+            <Box sx={() => ({ display: "flex" })}>
+              <Box sx={() => ({ flex: "1" })}>
+                <Image src="/logo.png" alt="logo" width={100} height={40} />
+              </Box>
+              {!user && (
+                <>
+                  <Link href="/auth/login" passHref>
+                    <Anchor ml="lg" mr="lr">
+                      Login
+                    </Anchor>
+                  </Link>
+                  <Link href="/auth/register" passHref>
+                    <Anchor ml="lg" mr="lr">
+                      Register
+                    </Anchor>
+                  </Link>
+                </>
+              )}
+              {user && <UploadVideo />}
             </Box>
-            {!user && (
-              <>
-                <Link href="/auth/login" passHref>
-                  <Anchor ml="lg" mr="lr">
-                    Login
-                  </Anchor>
-                </Link>
-                <Link href="/auth/register" passHref>
-                  <Anchor ml="lg" mr="lr">
-                    Register
-                  </Anchor>
-                </Link>
-              </>
-            )}
-            {user && <UploadVideo/>}
-          </Box>
-        </Header>
-      }
-    >
-      {children}
-    </AppShell>
+          </Header>
+        }
+      >
+        {children}
+      </AppShell>
+    </VideoContextProvider>
   );
 }
 
