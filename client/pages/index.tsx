8f750a6dd14a0393a53a083f07c4cbfefd7246b4
onur <ionuraygan@gmail.com>
2022-11-21 17:56:11 +0300

    video teaser

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,17 +1,27 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import HomePageLayout from '../layouts/Home'
-import styles from '../styles/Home.module.css'
-import { ReactElement } from 'react'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import HomePageLayout from "../layouts/Home";
+import styles from "../styles/Home.module.css";
+import { ReactElement } from "react";
+import { useVideo } from "../context/videos";
+import { VideoTeaser } from "../components/VideoTeaser";
+import { SimpleGrid } from "@mantine/core";
 
-const Home  = () => {
+const Home = () => {
+  const { videos } = useVideo();
   return (
-    <div className={styles.container}></div>
-  )
-}
+    <div className={styles.container}>
+      <SimpleGrid cols={3}>
+        {(videos || []).map((video) => {
+          return <VideoTeaser key={video.videoId} video={video} />;
+        })}
+      </SimpleGrid>
+    </div>
+  );
+};
 
-Home.getLayout = function(page: ReactElement){
-  return <HomePageLayout>{page}</HomePageLayout>
-} 
-export default Home
+Home.getLayout = function (page: ReactElement) {
+  return <HomePageLayout>{page}</HomePageLayout>;
+};
+export default Home;
